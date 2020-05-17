8687b9723e14145e7add5d2fb798e8e606552519
Falier77777 <freik.aa@phystech.edu>
2020-05-17 23:48:28 +0300

    add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Minimalio',
+      version='0.1',
+      description='The coolest game in the world',
+      url='https://github.com/Unicorn-Dev/Minimal.io',
+      author='Unicorn-Dev',
+      author_email='Unicorn-Dev@example.com',
+      license='MIT',
+      packages=['Application'],
+      zip_safe=False)
